db6c61db0faa88933f3ea0b7a0785bdc2fbc6bb6
leung7963 <104255180+leung7963@users.noreply.github.com>
2024-09-08 03:30:37 +0800

    更新 dnscf.py

--- dnscf.py
+++ dnscf.py
@@ -79,7 +79,7 @@ else:
                 body={
                     "name": domain_name + ".",
                     "type": "A",
-                    "ttl": 3600,
+                    "ttl": 300,
                     "records": [ip],
                     "weight": "1"
                 }
